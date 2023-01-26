71798d0188145dd9aee2bc142446f95079537f4d
Ruchitha Chinta <122854121+ruchitha36@users.noreply.github.com>
2023-01-26 12:24:57 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,72 @@
+"""
+Day:Wednesday
+Date:25th Jan 2023
+Topic:Strings, isspace method,type conversion,concatenation. 
+Description:String is a data type which are enclosed in quotes.
+isspace()-->using isspace() method-->isspace gives true if there is any space in the string and gives false when there is something in the string.
+Type conversion-->Converts one data type to other type.
+Concatenation--> Combining two strings using the + symbol.
+"""
+
+# a=3
+# print("The value of a is",a)
+# print("The value of a is ",a)
+
+'''isspace() method'''
+
+# b=""
+# print(b,type(b))
+
+# c=""
+# print(c.isspace())
+
+# c="."
+# print(c.isspace())
+
+# c=" "
+# print(c.isspace())
+# c="     "
+# print(c.isspace())
+
+'''Type conversion'''
+
+# d=5
+# print(type(d))
+# print(int(d))
+# print(float(d))
+# print(str(d))
+
+# b=7.8
+# print(type(b))
+# print(float(b))
+# print(int(b))
+# print(str(b))
+
+# d="12"
+# print(d,type(d))
+# print(int(d))
+# print(float(d))
+
+
+# print(int(5.6))
+# print(float(3))
+# print(int("python"))
+# print(float("program"))
+                                #error because int ot float cannot directly convert into strings.
+# e='67.45'
+# print(e,type(e))
+# g=float(e) 
+# print(g)
+# print(int(e))
+# print(int(g))
+
+
+'''Concatenation of strings'''
+
+# d='DELL'
+# print("One of the Laptop Brand is",d)
+# print(''+d)
+# print(' '+d)
+# print("One of the Laptop Brand is"+d)
+# print("One of the Laptop Brand is"+'   '+d)
+
---
+++ type _conversion.py
@@ -0,0 +1,9 @@
+# a=4
+# print(type(a))
+# print(float(a))
+# print(str(a))
+
+# b=6.8
+# print(type(b))
+# print(int(b))
+# print(str(b))
